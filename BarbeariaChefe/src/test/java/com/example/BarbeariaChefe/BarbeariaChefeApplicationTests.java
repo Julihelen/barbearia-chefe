f483358a821de16f296a045bcc2a13e049f54e4d
unknown <juliahlsantanaa@gmail.com>
2025-04-28 20:02:33 -0300

    Change: First commit

---
+++ BarbeariaChefe/src/test/java/com/example/BarbeariaChefe/BarbeariaChefeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.BarbeariaChefe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BarbeariaChefeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
